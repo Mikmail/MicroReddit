6c43165ac4d156568e2f1fe51de921a30a6aa6f8
Mikmail <mikael@rund.no>
2022-09-28 14:53:01 +0200

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+
+  validates :username, presence: true, uniqueness: true, length: {minimum: 5 }
+  validates :password, presence: true, length: {minimum: 8 }
+end
